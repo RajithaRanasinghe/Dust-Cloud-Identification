0e0473055a54619648e5e6ceed345418d57f0a1d
Rajitha Gayan Ranasinghe <rajitha.g.ranasinghe@gmail.com>
2022-11-10 17:14:06 +1100

    update-2022-11-10

---
+++ main.py
@@ -0,0 +1,263 @@
+from PySide2.QtGui import QPixmap,QPalette,QColor,QPainter, QPen, QBrush, QImage, QIcon, QIntValidator, QDoubleValidator
+from PySide2.QtWidgets import QWidget,QComboBox,QDialog, QCheckBox, QMessageBox, QFileDialog, QPushButton, QLineEdit, QProgressBar, QGridLayout, QHBoxLayout, QVBoxLayout, QApplication, QSplashScreen,QTabWidget,QMainWindow,QLabel,QAction,QStyleFactory,QDialogButtonBox,QStackedLayout
+from PySide2.QtCore import QTimer, Qt, QSize, QRunnable, Slot, QThreadPool, QObject, Signal
+
+from datetime import datetime
+import os
+import csv
+import sys
+import numpy as np
+from PIL import Image
+import test
+
+
+from torchvision.models.segmentation import lraspp_mobilenet_v3_large, fcn_resnet101, deeplabv3_mobilenet_v3_large
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+
+class DustIdentifier(QWidget):
+    def __init__(self):
+        super(DustIdentifier, self).__init__()
+
+        self.ICON_NAME = 'icon.png'
+        self.Init_diagram = 'diagram.png'
+        self.FOLDER_NAME = '/app_data'
+        self.Output_FOLDER = '/Output'
+        
+        self.setWindowTitle("Dust Cloud Identifier V0.1")
+        ICON_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)) + self.FOLDER_NAME, self.ICON_NAME)
+        
+        self.DIAGRAM_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)) + self.FOLDER_NAME, self.Init_diagram)
+        self.setWindowIcon(QIcon(ICON_PATH))
+
+        self.Ml_ModelList = {'FCN-Resnet101' : fcn_resnet101, 'DeepLabV3-MobileNet-V3-Large' : deeplabv3_mobilenet_v3_large, 'Lraspp-MobileNet-V3-Large' : lraspp_mobilenet_v3_large}
+        self.Ml_TrainedModels = {'FCN-Resnet101' : 'FCN-Resnet101_dataset_897.pth', 'DeepLabV3-MobileNet-V3-Large' : 'DeepLabV3-MobileNet-V3-Large_dataset_897.pth', 'Lraspp-MobileNet-V3-Large' : 'Lraspp-MobileNet-V3-Large_dataset_897.pth'}
+        self.Ml_TrainedModelURLs = {'FCN-Resnet101' : 'https://drive.google.com/file/d/1GFhZq4qzGj-PXiZDlZf66goDAa_SzGMw/view?usp=share_link', 'DeepLabV3-MobileNet-V3-Large' : 'https://drive.google.com/file/d/1GEroYQmlq9HoxT-id8PyAaXLfg706GzE/view?usp=share_link', 'Lraspp-MobileNet-V3-Large' : 'https://drive.google.com/file/d/1GGO-SO9eMQdwxhA-4gYdoI0-5AmE3ryp/view?usp=share_link'}
+        self.Ml_TrainedModelIDs = {'FCN-Resnet101' : '1GFhZq4qzGj-PXiZDlZf66goDAa_SzGMw', 'DeepLabV3-MobileNet-V3-Large' : '1GEroYQmlq9HoxT-id8PyAaXLfg706GzE', 'Lraspp-MobileNet-V3-Large' : '1GGO-SO9eMQdwxhA-4gYdoI0-5AmE3ryp'}
+        self.parameters = {'InputPath':'', 'InputType':'', 'InputName':'', 'ModelInputSize':256, 'OutputPath':'', 'CSVPath':'', 'TrainedDataset':'URDE_dataset_100', 'Model':0, 'ModelName':'', 'ModelPath':'', 'ModelURL':'', 'TrainedModel':'', 'ModelID':''}
+        
+
+        self.threadpool = QThreadPool()
+
+        self.initUI()
+        self.showImages(Image.open(self.DIAGRAM_PATH), [], 'Process')
+
+
+    def show_view(self, image):
+        self.showImages(image, [], 'Identifying ..')
+
+
+    def Run(self):
+        worker = test.Worker(self.parameters)
+        worker.Network_inst.result.connect(self.show_view)
+        worker.Network_inst.cycleStart.connect(self.clearView)
+        worker.Network_inst.progress.connect(self.setProgress)
+        self.threadpool.start(worker)
+
+    def initUI(self):
+        #Layouts
+        self.main_layout = QVBoxLayout()
+        self.control_btn_layout = QHBoxLayout()
+        self.imgView_layout = QStackedLayout()
+
+        #create components for layout
+        self.createUIcomponents()
+
+        #set margins (left,top,right,bottom)
+        self.main_layout.setContentsMargins(1,1,1,1)
+        self.imgView_layout.setContentsMargins(1,1,1,1)
+        self.control_btn_layout.setContentsMargins(1,1,1,1)
+
+        #layout Widgets
+        self.main_widget = QWidget()
+        self.control_btn_widget = QWidget()
+        self.imgView_widget = QWidget()
+
+        #Add component widgets to button layout
+        self.control_btn_layout.addWidget(self.OpenVideo_btn)
+        self.control_btn_layout.addWidget(self.OpenImage_btn)
+        self.control_btn_layout.addWidget(self.Model_label)
+        self.control_btn_layout.addWidget(self.Model_cbx)
+        self.control_btn_layout.addWidget(self.about_btn)
+
+        #Set widget
+        self.control_btn_widget.setLayout(self.control_btn_layout)
+
+        #Set widget
+        self.imgView_widget.setLayout(self.imgView_layout)
+
+
+        #Add widjets to main layout
+        self.main_layout.addWidget(self.control_btn_widget)
+        self.main_layout.addWidget(self.InputType_label)
+        self.main_layout.addWidget(self.InputPath_label)
+        self.main_layout.addWidget(self.ModelPath_label)
+        self.main_layout.addWidget(self.OutputPath_label)
+        self.main_layout.addWidget(self.imgView_widget)
+        self.main_layout.addWidget(self.Run_btn)
+        self.main_layout.addWidget(self.pbar)
+
+        #Set widget
+        #self.main_widget.setLayout(self.main_layout)
+
+        self.setLayout(self.main_layout)
+
+        
+
+    def createUIcomponents(self):
+        #Controls
+        self.OpenImage_btn = QPushButton("Load Image")
+        self.OpenImage_btn.setObjectName("OpenImage")
+        self.OpenImage_btn.clicked.connect(self.OpenImageFile)
+
+
+        self.OpenVideo_btn = QPushButton("Load Video")
+        self.OpenVideo_btn.setObjectName("OpenVideo")
+        self.OpenVideo_btn.clicked.connect(self.OpenVideoFile)
+
+        self.Run_btn = QPushButton("RUN")
+        self.Run_btn.setObjectName("RUN")
+        self.Run_btn.clicked.connect(self.Run)
+
+        self.about_btn = QPushButton("About")
+        self.about_btn.setObjectName("About")
+        self.about_btn.clicked.connect(self.aboutpressed)
+
+        self.Model_label = QLabel("Select ML Model")
+        self.Model_cbx = QComboBox()
+        for key in self.Ml_ModelList.keys():
+            self.Model_cbx.addItem(key)
+        self.Model_cbx.activated.connect(self.updateUI)
+
+        self.InputType_label = QLabel("File Type = {}".format(self.parameters['InputType']))
+        self.InputPath_label = QLabel("Input Path = {}".format(self.parameters['InputPath']))
+        self.OutputPath_label = QLabel("Output Path = {}".format(self.parameters['OutputPath']))
+        self.ModelPath_label = QLabel("Model Path = {}".format(self.parameters['ModelPath']))
+
+        #Progress Bar
+        self.pbar = QProgressBar()
+        self.pbar.setObjectName('ProgressBar')
+        self.pbar.setMaximum(100)
+
+
+    def clearView(self):
+        if self.imgView_layout is not None:
+            while self.imgView_layout.count():
+                item = self.imgView_layout.takeAt(0)
+                widget = item.widget()
+                if widget is not None:
+                    widget.deleteLater()
+                else:
+                    self.clearView(item.layout())  #recursive
+    
+    def showImages(self, Input_images, Output_Image, Method_title):
+
+        fig = Figure(figsize=(12, 12), dpi=65, facecolor=(1, 1, 1), edgecolor=(0, 0, 0))
+        fig.suptitle(Method_title, fontsize=10)
+
+        if np.shape(Input_images)[0] > 0 and np.shape(Output_Image)[0] > 0:
+            a=fig.add_subplot(1, 2, 1)
+            a.imshow(Input_images)
+
+            a=fig.add_subplot(1, 2, 2)
+            a.imshow(Output_Image)   
+
+        elif np.shape(Input_images)[0] > 0:
+            a=fig.add_subplot(1, 1, 1)
+            a.imshow(Input_images)
+   
+
+        fig.subplots_adjust(
+                    left=0.05,
+                    bottom=0.05, 
+                    right=0.95, 
+                    top=0.95, 
+                    wspace=0.1, 
+                    hspace=0.1)
+        
+
+        canvas = FigureCanvas(fig)
+        self.imgView_layout.addWidget(canvas)
+
+    def updateUI(self):
+        self.InputType_label.setText("File Type = {}".format(self.parameters['InputType']))
+        self.InputPath_label.setText("Input Path = {}".format(self.parameters['InputPath']))
+        
+
+        self.parameters['ModelName'] = self.Model_cbx.currentText()
+        self.parameters['TrainedModel'] =  self.Ml_TrainedModels[self.parameters['ModelName']]
+        MODEL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.parameters['TrainedModel'])
+        self.parameters['ModelPath'] = MODEL_PATH
+        self.parameters['ModelURL'] = self.Ml_TrainedModelURLs[self.parameters['ModelName']]
+        self.parameters['ModelID'] = self.Ml_TrainedModelIDs[self.parameters['ModelName']]
+
+        self.ModelPath_label.setText("Model Path = {}".format(self.parameters['ModelPath']))
+
+        OUTPUT_NAME = "output_" + self.parameters['ModelName'] + "_" + self.parameters['TrainedDataset'] + "_" + os.path.splitext(self.parameters['InputName'])[0]
+        OUTPUT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)) + self.Output_FOLDER, OUTPUT_NAME)
+        self.parameters['OutputPath'] = OUTPUT_PATH
+        self.OutputPath_label.setText("Output Path = {}".format(self.parameters['OutputPath']))
+        self.parameters['Model'] = self.Ml_ModelList[self.parameters['ModelName']]
+
+        CSV_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)) + self.Output_FOLDER, OUTPUT_NAME + '.csv')
+        self.parameters['CSVPath'] = CSV_PATH
+
+    def OpenVideoFile(self):
+        FILE_NAME, FILE_PATH = self.getFile()
+        self.parameters['InputName'] = FILE_NAME
+        self.parameters['InputType'] = 'Video'
+        self.parameters['InputPath'] = FILE_PATH
+        self.updateUI()
+
+
+    def OpenImageFile(self):
+        FILE_NAME, FILE_PATH = self.getFile()
+        self.parameters['InputName'] = FILE_NAME
+        self.parameters['InputType'] = 'Image'
+        self.parameters['InputPath'] = FILE_PATH
+        self.updateUI()
+
+
+    def getFile(self):
+        dlgBox = QFileDialog()
+        dlgBox.setFileMode(QFileDialog.AnyFile)
+        FILE_PATH,_ = dlgBox.getOpenFileName(self, 'Open File')
+        FILE_NAME = FILE_PATH.split('/')[-1]
+        return FILE_NAME, FILE_PATH
+
+    def setProgress(self,value):
+        self.pbar.setValue(value)
+
+
+    def aboutpressed(self):
+        message = QMessageBox()
+        message.setWindowTitle("ABOUT")
+        link = "https://github.com/RajithaRanasinghe?tab=repositories"
+        message.setText('<font color="blue">Written by</font><font color="red"> RGR </font><font color="green">!</font><br><a href="url">{}</a>'.format(link))
+
+        message.show()
+        message.exec_()
+
+    def Notice(self, notice):
+        message = QMessageBox()
+        message.setWindowTitle("NOTICE")
+        message.setText(str(notice))
+
+        message.show()
+        message.exec_()
+
+
+
+
+if __name__ == "__main__":
+    sys.argv.append('--no-sandbox')
+    app = QApplication(sys.argv)
+    QApplication.processEvents()
+    widget = DustIdentifier()
+    app.setApplicationName("Dust Cloud Identifier")
+    widget.show()
+
+    app.exec_()
+    sys.exit()
---
+++ test.py
@@ -0,0 +1,317 @@
+from PySide2.QtCore import QRunnable, Slot, QThreadPool, QObject, Signal
+from torchvision.transforms import transforms
+import requests
+import torch
+import cv2
+from PIL import Image
+import numpy as np
+import csv
+
+
+
+class Worker(QRunnable):
+
+
+    def __init__(self, parameters):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.Network_inst = Network(parameters)
+
+    @Slot()  # QtCore.Slot
+    def run(self):
+         self.Network_inst.run()
+
+
+class Network(QObject):
+    progress = Signal(float)
+    result = Signal(object)
+    cycleStart = Signal()
+
+    def __init__(self, parameters):
+        super(Network, self).__init__()
+        self.MyParameters = parameters
+
+
+    def run(self):
+        model = self.MyParameters['Model'](pretrained=False, progress=True, num_classes=1, aux_loss=None)
+        
+        
+        model.eval()
+        try:
+            model.cuda()
+        except:
+            model.cpu()
+
+
+        self.model_name = self.MyParameters['ModelName']
+        self.dataset_name = self.MyParameters['TrainedDataset']
+        self.model_path = self.MyParameters['ModelPath']
+        self.input_name = self.MyParameters['InputName']
+        self.model_url = self.MyParameters['ModelURL']
+        self.trainedModel_name = self.MyParameters['TrainedModel']
+        self.output_path = self.MyParameters['OutputPath']
+        self.input_type = self.MyParameters['InputType']
+        self.input_path = self.MyParameters['InputPath']
+        self.csv_path = self.MyParameters['CSVPath']
+        self.model_id = self.MyParameters['ModelID']
+
+
+        csv_header = ['File Name', 'Model Name', 'Frame', 'Dust Pixel Ratio', 'PM30', 'PM10', 'PM4', 'PM2.5', 'PM1']
+
+        print(self.csv_path)
+        csv_file = open(self.csv_path, 'w')
+        writer = csv.writer(csv_file)
+        writer.writerow(csv_header)
+
+
+        try:
+            model.load_state_dict(torch.load(self.model_path))
+        except:
+            response = requests.get(self.model_url)
+            open(self.trainedModel_name, "wb").write(response.content)
+            #self.download_file_from_google_drive(self.model_id, '/Output')
+            model.load_state_dict(torch.load(self.model_path))
+
+        if self.input_type == 'Video':
+
+            vs = cv2.VideoCapture(self.input_path)
+            _, frame = vs.read()
+            H, W, _ = frame.shape
+            vs.release()
+
+            fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+            out = cv2.VideoWriter(self.output_path, fourcc, 24, (W, H), True)
+
+            cap = cv2.VideoCapture(self.input_path)
+            totalframecount= int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+            idx = 0
+            img_transform = transforms.Compose([
+                transforms.Resize((256,256)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
+
+            dust_array =[]
+
+            while True:
+                ret, frame = cap.read()
+                if ret == False:
+                    cap.release()
+                    out.release()
+                    break
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                H, W, _ = frame.shape
+                ori_frame = frame
+                ori_frame1 = Image.fromarray(ori_frame)
+                frame = img_transform(ori_frame1)
+                input = frame.unsqueeze(0).float()
+                input = input.cuda()
+                mask = torch.sigmoid(model(input)['out'])
+                mask = mask.squeeze(0).squeeze(0) 
+
+                #Dust Probability clip   
+                #mask = mask > 0.99
+
+
+                mask = mask.detach().cpu().numpy()       
+                mask = mask.astype(np.float32)
+                mask = cv2.resize(mask, (W, H))
+                mask = np.expand_dims(mask, axis=-1)
+
+                
+                dust_array.append(np.sum(mask))
+
+                invmask = mask.copy()
+                invmask = abs(mask - 1)
+                combine_frame = ori_frame
+
+                combine_frame[:,:,1] = combine_frame[:,:,1] * invmask[:,:,0]
+                combine_frame = combine_frame.astype(np.uint8)
+                total_pixels = (combine_frame.shape[0] * combine_frame.shape[1])
+
+                if len(dust_array) > 30:
+                    dust_pixel = (np.sum(dust_array[-31:-1]))/30
+                else:
+                    dust_pixel = (np.sum(dust_array))/len(dust_array)
+
+                #print('total = {:.1f} dust = {:.1f}'.format(total_pixels,dust_pixel))
+                dustRatio = (dust_pixel/total_pixels)*100
+                pm30 = 0.675 * (dustRatio) + 2.7967
+                pm10 = 0.6764 * (dustRatio) + 2.0064
+                pm4 = 0.5385 * (dustRatio) + 1.1539
+                pm2_5 = 0.4709 * (dustRatio) + 0.9569
+                pm1 = 0.4501 * (dustRatio) + 0.9037
+
+
+                csv_data = [self.input_name, self.model_name, idx, dustRatio, pm30, pm10, pm4, pm2_5, pm1]
+                writer.writerow(csv_data)
+
+                dust_data = ['PM30 = {:.1f}'.format(pm30), 'PM10 = {:.1f}'.format(pm10), 'PM4 = {:.1f}'.format(pm4),'PM2.5 = {:.1f}'.format(pm2_5),'PM1 = {:.1f}'.format(pm1)]
+                
+                i = 0
+                for data in dust_data:
+                    cv2.putText(combine_frame, 
+                        data,
+                        (50, 50 +i), 
+                        font, 1, 
+                        (10, 10, 255-i),
+                        4, 
+                        cv2.LINE_4)
+                    i+= 50
+                
+                model_data = ['Model = {}'.format(self.model_name), self.dataset_name, 'Dust Pixel Ratio% = {:.1f}'.format(dustRatio)]
+                
+                for data in model_data:
+                    cv2.putText(combine_frame, 
+                        data,
+                        (50, 50 +i), 
+                        font, 1, 
+                        (255, 0, 255),
+                        4, 
+                        cv2.LINE_4)
+                    i+= 50
+
+                self.cycleStart.emit()
+                self.result.emit(cv2.cvtColor(combine_frame, cv2.COLOR_BGR2RGB))
+                idx += 1
+                pgr = (idx / totalframecount) * 100
+                self.progress.emit(pgr)
+
+
+                out.write(combine_frame)
+            print('All Done')
+            cap.release()
+
+
+        if self.input_type == 'Image':
+            totalframecount = 1
+            idx = 0
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            img_transform = transforms.Compose([
+                transforms.Resize((256,256)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
+
+            dust_array =[]
+
+            ori_frame = Image.open(self.input_path)
+            (H, W, _) = np.shape(ori_frame)       
+            frame = img_transform(ori_frame)
+            input = frame.unsqueeze(0).float()
+            input = input.cuda()
+            mask = torch.sigmoid(model(input)['out'])
+            mask = mask.squeeze(0).squeeze(0) 
+
+            #Dust Probability clip   
+            #mask = mask > 0.99
+
+            mask = mask.detach().cpu().numpy()       
+            mask = mask.astype(np.float32)
+            mask = cv2.resize(mask, (W, H))
+            mask = np.expand_dims(mask, axis=-1)
+          
+            dust_array.append(np.sum(mask))
+
+            invmask = mask.copy()
+            invmask = abs(mask - 1)
+            combine_frame = np.asanyarray(ori_frame)
+
+            print(type(ori_frame))
+
+            combine_frame[:,:,1] = combine_frame[:,:,1] * invmask[:,:,0]
+            combine_frame = combine_frame.astype(np.uint8)
+            total_pixels = (combine_frame.shape[0] * combine_frame.shape[1])
+
+            if len(dust_array) > 30:
+                dust_pixel = (np.sum(dust_array[-31:-1]))/30
+            else:
+                dust_pixel = (np.sum(dust_array))/len(dust_array)
+
+            #print('total = {:.1f} dust = {:.1f}'.format(total_pixels,dust_pixel))
+            dustRatio = (dust_pixel/total_pixels)*100
+            pm30 = 0.675 * (dustRatio) + 2.7967
+            pm10 = 0.6764 * (dustRatio) + 2.0064
+            pm4 = 0.5385 * (dustRatio) + 1.1539
+            pm2_5 = 0.4709 * (dustRatio) + 0.9569
+            pm1 = 0.4501 * (dustRatio) + 0.9037
+
+
+            csv_data = [self.input_name, self.model_name, idx, dustRatio, pm30, pm10, pm4, pm2_5, pm1]
+            writer.writerow(csv_data)
+
+            dust_data = ['PM30 = {:.1f}'.format(pm30), 'PM10 = {:.1f}'.format(pm10), 'PM4 = {:.1f}'.format(pm4),'PM2.5 = {:.1f}'.format(pm2_5),'PM1 = {:.1f}'.format(pm1)]
+
+            '''           
+            i = 0
+            for data in dust_data:
+                cv2.putText(combine_frame, 
+                    data,
+                    (480, 50 +i), 
+                    font, 1, 
+                    (10, 10, 255-i),
+                    4, 
+                    cv2.LINE_4)
+                i+= 50
+            
+            model_data = ['Model = {}'.format(self.model_name), self.dataset_name, 'Dust Pixel Ratio% = {:.1f}'.format(dustRatio)]
+            
+            i = 0
+            for data in model_data:
+                cv2.putText(combine_frame, 
+                    data,
+                    (50, 50 +i), 
+                    font, 1, 
+                    (255, 0, 255),
+                    4, 
+                    cv2.LINE_4)
+                i+= 50
+
+            ''' 
+
+            self.cycleStart.emit()
+            self.result.emit(combine_frame)
+            idx += 1
+            pgr = (idx / totalframecount) * 100
+            self.progress.emit(pgr)
+
+            print('All Done')
+
+
+
+        csv_file.close()
+
+    def download_file_from_google_drive(self, id, destination):
+        URL = "https://docs.google.com/uc?export=download"
+
+        session = requests.Session()
+
+        response = session.get(URL, params = { 'id' : id }, stream = True)
+        token = self.get_confirm_token(response)
+
+        if token:
+            params = { 'id' : id, 'confirm' : token }
+            response = session.get(URL, params = params, stream = True)
+
+        self.save_response_content(response, destination)    
+
+    def get_confirm_token(self, response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(self, response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+
+
+        
+
+        
